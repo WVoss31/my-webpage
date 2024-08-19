3f6eae60d6aa17d68ccfad2d10ce60a4b724b84f
Walker Voss <vosswalker@gmail.com>
2024-08-19 00:35:21 -0500

    added misc stuff

---
+++ src/pages/Education.jsx
@@ -0,0 +1,9 @@
+const Education = () => {
+  return (
+    <div>
+        <h1 className="text-5xl font-bold mb-4">This is a Test Page</h1>
+        <p className="text-xl">To Test the Routing and navigation</p>
+    </div>
+  )
+}
+export default Education
