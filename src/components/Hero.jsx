07285f4318d390dde2db1d5fd0b53717f3eb4aa4
Walker Voss <vosswalker@gmail.com>
2024-10-02 13:48:42 -0500

    Updated Home

---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types'
+import { Button } from "@nextui-org/react"
+
+const Hero = ({title, subtitle, action}) => {
+  return (
+    <div
+        className="h-screen bg-cover bg-center flex items-center justify-center text-white"
+        style={{ backgroundImage: "url(src/assets/space.jpg)" }}
+      >
+        <div className="text-center">
+          <h1 className="text-5xl font-bold mb-4">{title}</h1>
+          <p className="text-xl mb-8">{subtitle}</p>
+          <Button color="primary" variant="ghost">
+            {action}
+          </Button>
+        </div>
+      </div>
+  )
+}
+
+
+Hero.propTypes = {
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    action: PropTypes.string.isRequired,
+  };
+
+export default Hero
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,42 +1,20 @@
-import { useEffect, useState } from "react";
+import { useRef } from "react";
 import { Button } from "@nextui-org/react";
+import Hero from "../components/Hero";
+import { useIsVisible } from "../helpers/IsIntersecting";
 
 const Home = () => {
-  const [scrollY, setScrollY] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrollY(window.scrollY / 2);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+  const ref = useRef();
+  const isVisible = useIsVisible(ref);
 
   return (
     <>
-    {/* Beginning of Hero Page */}
-      <div
-        className="h-screen bg-cover bg-center flex items-center justify-center text-white"
-        style={{ backgroundImage: "url(src/assets/space.jpg)" }}
-      >
-        <div className="text-center">
-          <h1 className="text-5xl font-bold mb-4">Welcome to My Website</h1>
-          <p className="text-xl mb-8">I make cool stuff</p>
-          <Button color="primary" variant="ghost">
-            About Me
-          </Button>
-        </div>
-      </div>
+      <Hero title="Welcome to My Website" subtitle="I make cool stuff" action="About Me" />
       {/* Beginning of Animated Cover Page */}
-      <div
-        className={`transition-all duration-500 ${
-          scrollY > window.innerHeight ? "relative top-0" : "relative"
-        }`}
-        style={{ top: `-${Math.min(scrollY, window.innerHeight)}px` }}
-      >
-        <div className="h-screen flex items-center justify-center bg-gray-100 dark:bg-slate-900">
-          <div className="text-center">
+        <div className="h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900">
+          <div ref={ref} className={`text-center transition-opacity ease-in duration-700 ${
+          isVisible ? "opacity-100" : "opacity-0"
+        }`}>
             <h2 className="text-4xl font-bold mb-4">About this Webpage</h2>
             <p className="text-xl mb-8 ml-4 mr-4">
               Read about the technology stack and development process
@@ -44,9 +22,10 @@ const Home = () => {
             <Button color="primary">View Devlog</Button>
           </div>
         </div>
-        {/* Put text Here */}
-        <h2 className="text-3xl bold">Test</h2>
-      </div>
+        <h2 className="text-4xl font-bold text-center mt-5">Preview My Projects</h2>
+        <div>
+          {/* Cards go Here */}
+        </div>
     </>
   );
 };
