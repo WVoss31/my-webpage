717dabbc5f2734bce6971c9154c506ffe08d04a4
Walker Voss <vosswalker@gmail.com>
2024-10-05 12:44:39 -0500

    Updates

--- src/pages/Projects.jsx
+++
@@ -1,9 +0,0 @@
-const Projects = () => {
-  return (
-    <div>
-        <h1 className="text-xl text-bold">This is a placholder</h1>
-        <p>For the Projects page</p>
-    </div>
-  )
-}
-export default Projects
